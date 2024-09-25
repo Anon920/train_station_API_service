a736006f1a0ea60b39530711e4b1bebed571a2ab
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-25 15:44:58 +0300

    Created class Train in models and addded them to admin

--- train/models.py
+++ train/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Train(models.Model):
+    name = models.CharField(max_length=100)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+
+    def __str__(self):
+        return f"Train: {self.name} (id {self.id})"
+
+    class Meta:
+        verbose_name_plural = "trains"
+
+
