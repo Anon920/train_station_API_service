65c0d004c018234d4c30f43d558ed9fe11222c82
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 15:15:43 +0300

    Created Train Type model

--- station/models.py
+++ station/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class TrainType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Train(models.Model):
     name = models.CharField(max_length=100)
     cargo_num = models.PositiveIntegerField()
