c1e3555d61b0a24c180fd635131543aba3a7ce0c
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 13:40:25 +0300

    created method POST in view.py

--- station/views.py
+++ station/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+from django.http import JsonResponse
 
 from station.models import Train
 from station.serializers import TrainSerializer
@@ -8,4 +8,5 @@ def train_list(request):
     if request.method == 'GET':
         trains = Train.objects.all()
         serializer = TrainSerializer(trains, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
+
