9bfaf993133dc8697245c2e9803d8bbbf85f25d6
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 15:09:25 +0300

    Added parameter places_in_cargo(forgot added them)

--- station/models.py
+++ station/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Train(models.Model):
     name = models.CharField(max_length=100)
-    cargo_num = models.IntegerField()
+    cargo_num = models.PositiveIntegerField()
+    places_in_cargo = models.PositiveIntegerField()
+    # train_type =
 
     def __str__(self):
         return f"Train: {self.name} (id {self.id})"
 
     class Meta:
         verbose_name_plural = "trains"
-
-
