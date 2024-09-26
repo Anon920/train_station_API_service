80e37416aeb3e0a136fe171c387aa36b45757af3
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 16:00:51 +0300

    Register Station in admin and created endpoint for it

--- station/admin.py
+++ station/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Train, TrainType
+from .models import Train, TrainType, Station
 
 admin.site.register(Train)
 admin.site.register(TrainType)
+admin.site.register(Station)
--- station/urls.py
+++ station/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import TrainViewSet, TrainTypeViewSet
+from station.views import TrainViewSet, TrainTypeViewSet, StationViewSet
 
 router = routers.DefaultRouter()
 router.register('trains', TrainViewSet)
 router.register('trains_types', TrainTypeViewSet)
+router.register('stations', StationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
