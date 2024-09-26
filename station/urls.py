3ec61e47a249c4483abbbf23468a75a04d038a55
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 15:18:04 +0300

    Register TrainType in admin.py and created endpoint for it

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import TrainViewSet
+from station.views import TrainViewSet, TrainTypeViewSet
 
 router = routers.DefaultRouter()
 router.register('trains', TrainViewSet)
+router.register('trains_types', TrainTypeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
