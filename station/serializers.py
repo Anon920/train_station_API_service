640a7a0f80be388e7be7105b85077399a35c9337
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 15:58:55 +0300

    Created Station Serializer

--- station/serializers.py
+++ station/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from station.models import Train, TrainType
+from station.models import Train, TrainType, Station
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
@@ -14,3 +14,9 @@ class TrainSerializer(serializers.ModelSerializer):
         model = Train
         fields = ("id", "name", "cargo_num", "places_in_cargo", "train_type")
         read_only_fields = ("id",)
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ("id", "name", "latitude", "longitude")
