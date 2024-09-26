40681e200bdcc20e1a4abcc798b369c454c502b1
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-26 13:56:51 +0300

    Added endpoint for detail view in urls

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from station.views import train_list
-
+from station.views import train_list, train_detail
 
 urlpatterns = [
     path("trains/", train_list, name="train-list"),
+    path("trains/<int:pk>/", train_detail, name="train-detail"),
 ]
 
 app_name = "station"
