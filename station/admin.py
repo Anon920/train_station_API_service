796528009e9b7211febc4680e4d235666eea16b2
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-30 12:12:33 +0300

    registered journey in admin.py and added endpoint

--- station/admin.py
+++ station/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Train, TrainType, Station, Route
+from .models import Train, TrainType, Station, Route, Journey
 
 admin.site.register(Train)
 admin.site.register(TrainType)
 admin.site.register(Station)
 admin.site.register(Route)
+admin.site.register(Journey)
+
--- station/urls.py
+++ station/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import TrainViewSet, TrainTypeViewSet, StationViewSet, RouteViewSet
+from station.views import TrainViewSet, TrainTypeViewSet, StationViewSet, RouteViewSet, JourneyViewSet
 
 router = routers.DefaultRouter()
 router.register('trains', TrainViewSet)
 router.register('trains_types', TrainTypeViewSet)
 router.register('stations', StationViewSet)
 router.register("routes", RouteViewSet)
+router.register("journey", JourneyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
