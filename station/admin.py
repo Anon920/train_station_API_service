a39f7acb3ea641da743f7afeec3a9569afc363e5
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-01 13:47:19 +0300

    Created endpoint for crew and register crew in admin.py

--- station/admin.py
+++ station/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Train, TrainType, Station, Route, Journey
+from .models import Train, TrainType, Station, Route, Journey, Crew
 
 admin.site.register(Train)
 admin.site.register(TrainType)
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Journey)
+admin.site.register(Crew)
 
--- station/urls.py
+++ station/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import TrainViewSet, TrainTypeViewSet, StationViewSet, RouteViewSet, JourneyViewSet
+from station.views import TrainViewSet, TrainTypeViewSet, StationViewSet, RouteViewSet, JourneyViewSet, CrewViewSet
 
 router = routers.DefaultRouter()
 router.register('trains', TrainViewSet)
@@ -9,6 +9,7 @@ router.register('trains_types', TrainTypeViewSet)
 router.register('stations', StationViewSet)
 router.register("routes", RouteViewSet)
 router.register("journey", JourneyViewSet)
+router.register("crew", CrewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
