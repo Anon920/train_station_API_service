467cdb8062b0a603fbc4d0d23a63d60fd9ccc3a0
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-03 16:25:42 +0300

    Edit Login, Create, Manager view and edit admin register

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token"),
+    path("login/", LoginUserView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="me")
 ]
 
