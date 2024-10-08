abc015cfd26f3cd2530ba5d8407b71bb9968dc39
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-08 23:31:30 +0300

    Created tests for station list/retrieve and train list/retreive

--- station/tests/test_train_station_api.py
+++ station/tests/test_train_station_api.py
@@ -7,26 +7,21 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from station.models import TrainType, Train, Station, Route, Crew, Journey
-from station.serializers import StationSerializer
+from station.serializers import StationSerializer, TrainListSerializer, TrainRetrieveSerializer
 
 STATION_URL = reverse("station:station-list")
 TRAIN_URL = reverse("station:train-list")
 
 
-def sample_train_type(**params):
-    defaults = {
-        "name": "Test train type"
-    }
-    defaults.update(params)
-    return TrainType.objects.create(**defaults)
-
-
 def sample_train(**params):
+    train_type_name = params.get("train_type_name", "Test train")
+    train_type, created = TrainType.objects.get_or_create(name=train_type_name)
+
     defaults = {
         "name": "Test train",
         "cargo_num": 150,
         "places_in_cargo": 10,
-        "train_type": sample_train_type()
+        "train_type": train_type,
     }
     defaults.update(params)
     return Train.objects.create(**defaults)
@@ -85,3 +80,61 @@ class UnauthenticatedTrainStationApiTests(TestCase):
     def test_auth_required_for_trains(self):
         res = self.client.get(TRAIN_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedTrainStationApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_station_list(self):
+        sample_station(name="Kyiv")
+        sample_station(name="Lviv")
+
+        res = self.client.get(STATION_URL)
+
+        stations = Station.objects.order_by("id")
+        serializer = StationSerializer(stations, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_station_detail(self):
+        station = sample_station(name="Kyiv")
+
+        url = reverse("station:station-detail", args=[station.id])
+
+        res = self.client.get(url)
+
+        serializer = StationSerializer(station)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_train_list(self):
+        sample_train()
+        sample_train()
+
+        res = self.client.get(TRAIN_URL)
+
+        trains = Train.objects.order_by("id")
+        serializer = TrainListSerializer(trains, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_train_detail(self):
+        train = sample_train()
+
+        url = reverse("station:train-detail", args=[train.id])
+
+        res = self.client.get(url)
+
+        serializer = TrainRetrieveSerializer(train)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
